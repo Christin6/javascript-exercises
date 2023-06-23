51b61049021c15555f7ee63701c0513bb1d1a181
Christin6 <94294443+Christin6@users.noreply.github.com>
2023-06-23 19:24:01 +0700

    complete exercises 01-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    let nArray = array;
+    for (let item of toRemove) {
+        if (nArray.includes(item)) {
+            nArray.splice(nArray.indexOf(item), 1);
+        } else {continue}
+    }
+    return nArray;
 };
 
 // Do not edit below this line
